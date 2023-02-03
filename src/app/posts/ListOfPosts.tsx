fa160da3abd0c42a1c4947355e2449bf3ff14984
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-03 16:48:24 -0500

    chore: add navigation

---
+++ src/app/posts/ListOfPosts.tsx
@@ -0,0 +1,14 @@
+const fetchPosts = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+}
+
+export async function ListOfPosts() {
+  const posts = await fetchPosts()
+
+  return posts.slice(0, 5).map(post => (
+    <article key={post.id}>
+      <h2 style={{ color: '#09f' }}>{post.title}</h2>
+      <p>{post.body}</p>
+    </article>
+  ))
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+import { ListOfPosts } from "./ListOfPosts";
+
+export default function PostsPage() {
+  return (
+    <section>
+      <ListOfPosts />
+    </section>
+  )
+}
