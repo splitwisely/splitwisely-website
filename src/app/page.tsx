723cffd98f7b5a4f789ca71253adf5e4ebb44848
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-10-11 11:31:15 -0500

    refactor: global css and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import styles from './page.module.css'
+
 export default function Home() {
   return (
-    <h1>This is Splitwisely</h1>
+    <div className={styles.main}>
+      <h1 className={styles.title}>This is Splitwisely</h1>
+      <p className={styles.description}>the best way to manage your expenses on your trip 🌎</p>
+    </div>
   )
 }
