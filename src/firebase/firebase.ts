80ef99a6706ad2712a7b4c2ec944031fb1c62248
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-10-16 08:33:33 -0500

    fix: environment variables

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,12 +3,13 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    })
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(
+        /\\n/g,
+        '\n'
+      ),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
